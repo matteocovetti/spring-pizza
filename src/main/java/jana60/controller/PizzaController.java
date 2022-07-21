0639007e6f387f88636364151015e337821a42da
matteocovetti <covetti.matteo@gmail.com>
2022-07-21 17:25:01 +0200

    add repo-controller

---
+++ src/main/java/jana60/controller/PizzaController.java
@@ -0,0 +1,21 @@
+package jana60.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jana60.repository.PizzaRepository;
+
+@Controller
+@RequestMapping("/")
+public class PizzaController {
+
+	@Autowired
+	private PizzaRepository repo;
+
+	@GetMapping
+	public String pizzaList() {
+		return "";
+	}
+}

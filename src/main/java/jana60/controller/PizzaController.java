a8ad6ab011edf15bc5358c8544ab3f239497de6d
matteocovetti <covetti.matteo@gmail.com>
2022-07-21 18:27:22 +0200

    esercizio finito-bonus finito

--- src/main/java/jana60/controller/PizzaController.java
+++ src/main/java/jana60/controller/PizzaController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,9 +34,13 @@ public class PizzaController {
 	}
 
 	@PostMapping("/add")
-	public String save(@Valid @ModelAttribute("pizza") Pizza formPizza) {
-		repo.save(formPizza);
-		return "redirect:/";
+	public String save(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult br) {
+		if (br.hasErrors()) {
+			return "/pizza/edit";
+		} else {
+			repo.save(formPizza);
+			return "redirect:/";
+		}
 
 	}
 }

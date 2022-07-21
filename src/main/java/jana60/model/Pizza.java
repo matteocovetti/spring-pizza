cd35d800664a942511fb1b261f8ec5f9a5ae6177
matteocovetti <covetti.matteo@gmail.com>
2022-07-21 17:18:45 +0200

    first commit-add model

---
+++ src/main/java/jana60/model/Pizza.java
@@ -0,0 +1,59 @@
+package jana60.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.validation.constraints.Min;
+
+@Entity
+public class Pizza {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	@Lob
+	private String descrizione;
+
+	@Min(value = 0)
+	private double prezzo;
+
+	// getter e setter
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+}
